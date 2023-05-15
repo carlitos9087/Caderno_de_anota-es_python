a4d797e75d344eb5250dcfee26e8344528ff9041
carlitos9087 <carloseduardogomesalves@gmail.com>
2023-05-15 20:00:58 -0300

    fix

---
+++ teste.py
@@ -0,0 +1,10 @@
+import PySimpleGUI as sg
+
+filename = sg.popup_get_file('Enter the file you wish to process')
+sg.popup('You entered', filename)
+from PySimpleGUI import *
+import tkinter as tk
+
+
+
+
